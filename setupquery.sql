da50db36364da802464e738ebb30f55c48b14985
elmalakomar <elmalakomar@gmail.com>
2016-05-31 22:56:07 +0200

    Created file setupquery.sql

---
+++ setupquery.sql
@@ -0,0 +1,13 @@
+-- FOODEXPRESS PROJECT --
+
+/*
+COMMANDS:
+\c nameDb --> connect to nameDb
+\d nameTable --> description of the table
+dropdb nameDb --> drop database 
+\dt --> List of tables of the database
+\l --> List of databases
+\!clear --> clear the console
+
+	
+*/
