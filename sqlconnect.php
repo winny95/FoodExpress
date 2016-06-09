f7fdaa0b889804909a673c218763ca71dde4a836
elmalakomar <elmalakomar@gmail.com>
2016-06-09 18:56:43 +0200

    Create connection to pgsql database with PDO

--- sqlconnect.php
+++ sqlconnect.php
@@ -1,4 +1,13 @@
 <?php
-	$dbconn = pg_connect("host=localhost port=5432 dbname=foodexpress");
-	echo $dbconn;
+	/*$dbconn = pg_connect("host=localhost port=5432 dbname=foodexpress");
+	echo $dbconn;*/
+    try {
+		  // connessione tramite creazione di un oggetto PDO
+		   $myPDO = new PDO('pgsql:host=localhost;dbname=foodexpress', 'Omar', '');
+
+	}
+	catch(PDOException $e) {
+		  // notifica in caso di errorre
+		  echo 'Error: '.$e->getMessage();
+	}
 ?>
